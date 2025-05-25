3979f524e6a9a6a7586dc1b9cb95811a147e5590
HristinaGjorgjievska <158847321+hristinagjorgjievska@users.noreply.github.com>
2025-05-25 23:35:28 +0200

    Initial commit

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -9,14 +9,12 @@ class SILab2Test {
     void testEveryStatement(){
         RuntimeException exception;
 
-        //Празна листа (allItems == null)
         List<Item> items = null;
         exception = assertThrows(RuntimeException.class, () -> {
             SILab2.checkCart(items, "4972946510274820");
         });
         assertEquals("allItems list can't be null!", exception.getMessage());
 
-        //Невалидно име (празен стринг)
         List<Item> items1 = new ArrayList<>();
         items1.add(new Item("", 10, 5, 0.9));
         exception = assertThrows(RuntimeException.class, () -> {
@@ -24,8 +22,6 @@ class SILab2Test {
         });
         assertEquals("Invalid item!", exception.getMessage());
 
-
-        //Погрешен формат на картичка (содржи букви)
         List<Item> items2 = new ArrayList<>();
         items2.add(new Item("item3", 3, 20, 0.5));
         exception = assertThrows(RuntimeException.class, () -> {
@@ -33,7 +29,6 @@ class SILab2Test {
         });
         assertEquals("Invalid character in card number!", exception.getMessage());
 
-        //Неточен број на картичка (не содржи 16 цифри)
         List<Item> items3 = new ArrayList<>();
         items3.add(new Item("item4", 5, 230, 2));
         exception = assertThrows(RuntimeException.class, () -> {
@@ -41,7 +36,6 @@ class SILab2Test {
         });
         assertEquals("Invalid card number!", exception.getMessage());
 
-        //Сите податоци се точно внесени
         List<Item> items4 = new ArrayList<>();
         items4.add(new Item("item5", 3, 350, 0.3));
         double checkCart2 = SILab2.checkCart(items4, "4972946510274820");
@@ -52,28 +46,24 @@ class SILab2Test {
     void testMultipleCondition(){
         String cardNumber = "4972946510274820";
 
-        //FFF
         List<Item> items1 = new ArrayList<>();
         Item item1 = new Item("item1", 10, 40, 0.5);
         items1.add(item1);
         double checkCart1 = SILab2.checkCart(items1, cardNumber);
         assertEquals(170, checkCart1, 0.1);
 
-        //FTX
         List<Item> items2 = new ArrayList<>();
         Item item2 = new Item("item2", 22, 232, 0.5);
         items2.add(item2);
         double checkCart2 = SILab2.checkCart(items2, cardNumber);
         assertEquals(2522, checkCart2, 0.0001);
 
-        //FFT
         List<Item> items3 = new ArrayList<>();
         Item item3 = new Item("item3", 11, 103, 0);
         items3.add(item3);
         double checkCart3 = SILab2.checkCart(items3, cardNumber);
         assertEquals(1103, checkCart3);
 
-        //TXX
         List<Item> items4 = new ArrayList<>();
         Item item4 = new Item("item4", 21, 732, 0.7);
         items4.add(item4);
